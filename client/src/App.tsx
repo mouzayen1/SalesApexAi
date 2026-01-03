fa282c45c4c4a80d77f7ea5753ad0972715c6c1c
Mohammad Mouzayen <mouzayen1@gmail.com>
2026-01-03 15:45:36 -0600

    Fix App.tsx router JSX + restore rehash-optimizer route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,15 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
-import RehashOptimizerPage from '@/pages/rehash-optimizer';
+import { Routes, Route } from "react-router-dom";
 
-function Router() {
+import Home from "./pages/home";
+import RehashOptimizerPage from "./pages/rehash-optimizer";
+import NotFound from "./pages/not-found";
+
+export default function App() {
   return (
-    <Switch>
-      <Route path="/" element={<Home} / />}
-      <Route element={N<otFound} / />}
-              <Route path="/rehash-optimizer" element={<RehashOptimizerPage} / />}
-    </Switch>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/rehash-optimizer" element={<RehashOptimizerPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;
