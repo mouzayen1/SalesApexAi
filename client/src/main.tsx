b14ae697c4278618b883b8dfaa16c5c9d9fde04b
Mohammad Mouzayen <mouzayen1@gmail.com>
2026-01-03 23:10:52 -0600

    Wrap App in BrowserRouter and add React imports

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
